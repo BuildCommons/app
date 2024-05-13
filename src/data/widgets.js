3695a2652ce40ef103d400faade135ea6b77215d
James Waugh <james@nearbuilders.org>
2024-05-13 05:38:17 -0400

    add src

---
+++ src/data/widgets.js
@@ -0,0 +1,49 @@
+const TestnetDomains = {
+  "test.nearbuilders.org": true,
+  "127.0.0.1": true,
+  "192.168.1.24": true,
+};
+
+export const NetworkId =
+  window.location.hostname in TestnetDomains ? "testnet" : "mainnet";
+
+const TestnetWidgets = {
+  default: "builddao.testnet/widget/app",
+  login: "builddao.testnet/widget/login",
+  trialAccountBanner: "builddao.testnet/widget/TrialAccountBanner",
+  onboardingFlow: "builddao.testnet/widget/OnboardingFlow",
+
+  image: "eugenethedream/widget/Image",
+  viewSource: "eugenethedream/widget/WidgetSource",
+  widgetMetadataEditor: "eugenethedream/widget/WidgetMetadataEditor",
+  widgetMetadata: "eugenethedream/widget/WidgetMetadata",
+  profileImage: "eugenethedream/widget/ProfileImage",
+  profilePage: "buildhub.near/widget/Profile",
+  profileName: "eugenethedream/widget/ProfileName",
+  profileInlineBlock: "eugenethedream/widget/Profile",
+  notificationButton: "eugenethedream/widget/NotificationButton",
+};
+
+const MainnetWidgets = {
+  default: "buildhub.near/widget/app",
+  login: "buildhub.near/widget/login",
+  trialAccountBanner: "buildhub.near/widget/TrialAccountBanner",
+  onboardingFlow: "buildhub.near/widget/OnboardingFlow",
+
+  image: "mob.near/widget/Image",
+  feed: "buildhub.near/widget/Feed",
+  resources: "buildhub.near/widget/Resources",
+  viewSource: "mob.near/widget/WidgetSource",
+  widgetMetadataEditor: "buildhub.near/widget/WidgetMetadataEditor",
+  widgetMetadata: "buildhub.near/widget/WidgetMetadata",
+  profileImage: "mob.near/widget/ProfileImage",
+  notificationButton: "mob.near/widget/NotificationButton",
+  profilePage: "buildhub.near/widget/Profile",
+  profileName: "patrick.near/widget/ProfileName",
+  editorComponentSearch: "mob.near/widget/Editor.ComponentSearch",
+  profileInlineBlock: "mob.near/widget/Profile.InlineBlock",
+  viewHistory: "bozon.near/widget/WidgetHistory",
+};
+
+export const Widgets =
+  NetworkId === "testnet" ? TestnetWidgets : MainnetWidgets;
---
+++ src/pages/JoinPage.js
@@ -0,0 +1,36 @@
+import { Widget } from "near-social-vm";
+import React from "react";
+import { useLocation } from "react-router-dom";
+import OnboardingFlow from "../components/OnboardingFlow";
+import { useBosLoaderStore } from "../stores/bos-loader";
+
+export default function JoinPage({ signedIn, widgets, ...passProps }) {
+  const redirectMapStore = useBosLoaderStore();
+  const location = useLocation();
+  const searchParams = new URLSearchParams(location.search);
+  const from = searchParams.get("from");
+
+  const CurrentView = signedIn
+    ? widgets.default
+    : from === "trial"
+      ? widgets.trialAccountBanner
+      : widgets.login;
+
+  console.log("CurrentView", CurrentView);
+  return (
+    <>
+      <OnboardingFlow signedIn={signedIn} widgets={widgets} />
+      <div className="h-100">
+        <Widget
+          src={CurrentView}
+          props={{
+            ...passProps,
+          }}
+          config={{
+            redirectMap: redirectMapStore.redirectMap,
+          }}
+        />
+      </div>
+    </>
+  );
+}
