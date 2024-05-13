6d4397dfff65085a0ca64f128340616b05e9bf41
James Waugh <james@nearbuilders.org>
2024-05-13 02:23:46 -0400

    page.home

--- apps/commons/widget/config/app.jsx
+++ apps/commons/widget/config/app.jsx
@@ -7,34 +7,6 @@ return {
       init: {
         name: "Home",
       },
-    },
-    feed: {
-      path: "${config_account}/widget/page.feed",
-      blockHeight: "final",
-      init: {
-        name: "Collab",
-      },
-    },
-    cohorts: {
-      path: "${config_account}/widget/page.cohorts",
-      blockHeight: "final",
-      init: {
-        name: "Cohorts",
-      },
-    },
-    states: {
-      path: "${config_account}/widget/page.states",
-      blockHeight: "final",
-      init: {
-        name: "CommonStates",
-      },
-    },
-    projects: {
-      path: "${config_account}/widget/page.projects",
-      blockHeight: "final",
-      init: {
-        name: "Projects",
-      },
-    },
+    }
   },
 };
